1be8f44c33f884f9633598110733b4c89e19a277
Kristjan Vool <kristjan@miros.ai>
2026-05-09 00:04:35 +0300

    fix: remove spurious eslint-disable comment in headers.ts

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function mergeHeaders(...sources: (HeadersInit | undefined)[]): Headers {
   const merged = new Headers()
   for (const source of sources) {
